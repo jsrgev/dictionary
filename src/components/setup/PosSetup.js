6659a57766f38ada8ee174d2c4b7a4216746bc1c
Josh Regev <jsrgev@gmail.com>
2021-12-05 23:13:16 +0200

    a little progress on class groups

--- src/components/setup/PosSetup.js
+++ src/components/setup/PosSetup.js
@@ -36,6 +36,16 @@ const PosSetup = props => {
     const posDefault = {name: "", abbr: "", multiChoice: false, gramClasses: []};
     const gramClassDefault = {name: "", abbr: "", gramForms: []};
 
+    const gramClassSetDefault = {
+        name: "",
+        gramForms: [
+            {
+                name: "",
+                abbr: "",
+            }
+        ],
+    };
+
     const addPos = () => {
         let setupCopy = clone(appState.setup);
         let setupCopyPath = _.get(setupCopy, pathFrag)
@@ -43,6 +53,14 @@ const PosSetup = props => {
         setAppState({setup: setupCopy});
     };
 
+
+    const addClassSet = index => {
+        let setupCopy = clone(appState.setup);
+        let setupCopyPath = _.get(setupCopy, pathFrag)
+        setupCopyPath[thisIndex].gramClassSets.splice(index+1, 0, clone(gramClassSetDefault));
+        setAppState({setup: setupCopy});
+        console.log(path[thisIndex].gramClassSets);
+    };
 
     const addGramClass = index => {
         let setupCopy = clone(appState.setup);
@@ -64,16 +82,16 @@ const PosSetup = props => {
 
     const popupItems = [
         ["Part of speech", addPos],
-        ["Class", () => addGramClass(path[thisIndex].gramClasses.length-1)],
+        ["Class set", () => addClassSet(path[thisIndex].gramClassSets.length-1)],
     ];
 
     const isAvailable = (a) => {
         return true;
     };
     
-    const isCurrentSelection = posName =>  {
-        console.log(path[thisIndex].gramClassSets);
-        console.log(posName);
+    const isCurrentSelection = (posName, i) =>  {
+        // console.log(path[thisIndex].gramClassSets[i]);
+        // console.log(posName);
         return false;
         // return path[thisIndex].name === posName;
     };
@@ -85,7 +103,7 @@ const PosSetup = props => {
     const isFirst = thisIndex === 0;
     const isLast = thisIndex === path.length-1;
 
-    // console.log(appState.setup.gramClassSets);
+    // console.log(path[thisIndex]);
 
     return(
         <>
@@ -112,20 +130,24 @@ const PosSetup = props => {
                     <input type="text" value={path[thisIndex].name} onChange={e => handleChange(e.target.value, "name")} />
                     <label>Abbreviation</label>
                     <input type="text" value={path[thisIndex].abbr} onChange={e => handleChange(e.target.value, "abbr")} />
-                    </div>
-                    <div className="row">
-                        <div className="row-controls">
+                </div>
+                { path[thisIndex].gramClassSets.length > 0 &&
+                    <>
+                        <div className="row">
+                            <div className="row-controls"></div>
+                            <div className="row-content">
+                                <div>Class group</div>
+                                <ul>
+                                    {
+                                        path[thisIndex].gramClassSets.map((a, i) => (
+                                            <li key={i} value={a.name} className={ isCurrentSelection(a.name, i) ? "selected" : isAvailable(a.name) ? ""  : "disabled" } onClick={selectClassSet}>{capitalize(a.name)}</li>
+                                        ))
+                                    }
+                                </ul>
+                            </div>
                         </div>
-                        <div className="row-content">
-                            <div>Class group</div>
-                            <ul>
-                                {
-                                    appState.setup.gramClassSets.map((a, i) => (
-                                        <li key={i} value={a.name} className={ isCurrentSelection(a.name) ? "selected" : isAvailable(a.name) ? ""  : "disabled" } onClick={selectClassSet}>{capitalize(a.name)}</li>
-                                    ))
-                                }
-                            </ul>
-                        </div>
+                    </>
+                }
                     {/* { path[thisIndex].gramClasses.length>1 &&
                         <>
                             <label>Classes Allowed</label>
@@ -135,7 +157,7 @@ const PosSetup = props => {
                             </ul>
                         </>
                     } */}
-               </div>
+               {/* </div> */}
                {/* { path[thisIndex].gramClasses.length>0 &&
                 path[thisIndex].gramClasses.map((a, i) => (
                     <GramClassSetup appState={appState} setAppState={setAppState} key={i} thisIndex={i} stringPath={stringPathA} prevIndentLevel={0} moveItem={moveItem} addGramClass={addGramClass} />
