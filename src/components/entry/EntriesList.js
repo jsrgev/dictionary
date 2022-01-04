13e7c5e49b145cb54d1d21a0ea70f86ccda34747
Josh Regev <jsrgev@gmail.com>
2022-01-04 14:01:03 +0200

    added filtering to entries list

--- src/components/entry/EntriesList.js
+++ src/components/entry/EntriesList.js
@@ -1,7 +1,14 @@
 import { sortEntries } from "../../utils";
+import {useState, useEffect} from 'react';
 
 const EntriesList = props => {
     const {state, setState, isDirty} = props;
+
+    const [filterTerm, changeFilterTerm] = useState("");
+
+    // useEffect(() => {
+
+    // }, filterTerm);
 
     const getSortedEntries = () => {
         const entrySet = state.allEntries.map(a => {
@@ -34,11 +41,19 @@ const EntriesList = props => {
 
     const isActive = id => id === state.entry._id;
 
+    // const sortedEntries = getSortedEntries();
+    const filteredEntries = getSortedEntries().filter(a => {
+        const entry = a.content.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+        const term = filterTerm.trim().normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+        return entry.includes(term);
+    });
+
     return (
         <div id="entries-list-section">
             <h2>Entries</h2>
+            <input type="text" value={filterTerm} onChange={e => changeFilterTerm(e.target.value)} />
             <ul id="entries-list">
-                {getSortedEntries().map((a, i) => (
+                {filteredEntries.map((a, i) => (
                     <li key={i} className={isActive(a.id) ? "active" : null} onClick={() => handleClick(a.id)}>{a.content}</li>
                 ))
 
